5ef8b161b1ae932c0688378f457c514e61dc4eed
Poorva Gokhale <poorvagokhale@Poorvas-MacBook-Pro.local>
2018-11-05 16:32:49 +0530

    ViuWeb Packages added. 1 Package have ViuSelenium Tests with single class. Another Package have testcases with proper Java class design

---
+++ Test/src/test/java/ViuWeb/HomePage.java
@@ -0,0 +1,24 @@
+package com.ViuWeb;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.openqa.selenium.Keys.ENTER;
+
+public class HomePage extends ViuPage{
+    public void setSearchVideoString(String searchVideoString) {
+        this.searchVideoString = searchVideoString;
+    }
+
+    String searchVideoString;
+    ViuPage searchPage1=new SearchPage();
+    public SearchPage searchVideo(WebDriver driver) {
+     //   searchVideoString = "Banned";
+        WebElement viuSearchBox = driver.findElement(By.className("txtsearch"));
+        viuSearchBox.sendKeys(searchVideoString);
+        viuSearchBox.sendKeys(ENTER);
+        return ((SearchPage)searchPage1);
+    }
+}
---
+++ Test/src/test/java/ViuWeb/ViuPage.java
@@ -0,0 +1,18 @@
+package com.ViuWeb;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ViuPage {
+    String viuURL;
+
+
+    public void setViuURL(String viuURL) {
+        this.viuURL = viuURL;
+    }
+
+
+
+
+}
