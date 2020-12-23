adacf9516526565a4192da23b5bedfb54d11b1fa
poorvaGokhale <poorve@gmail.com>
2020-12-23 14:39:38 +0530

    Updated ChromeDriver version issues

--- SeleniumTest/src/test/java/SeleniumTest/MailSignIn/SignIn.java
+++ SeleniumTest/src/test/java/SeleniumTest/MailSignIn/SignIn.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SignIn {
     @Test
     public void LaunchWeb(){
-        System.setProperty("webdriver.chrome.driver", "/Users/poorvagokhale/Downloads/chromedriver");
+     //   System.setProperty("webdriver.chrome.driver", "/Users/poorvagokhale/Downloads/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://login.yahoo.com/");
 
