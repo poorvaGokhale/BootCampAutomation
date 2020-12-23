164c7f35ef97c285eb4840ae30ac083ee45a8125
poorvaGokhale <poorve@gmail.com>
2020-12-23 12:05:57 +0530

    New class and Test for signing In to mail account

---
+++ SeleniumTest/src/test/java/SeleniumTest/MailSignIn/SignIn.java
@@ -0,0 +1,16 @@
+package SeleniumTest.MailSignIn;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SignIn {
+    @Test
+    public void LaunchWeb(){
+        System.setProperty("webdriver.chrome.driver", "/Users/poorvagokhale/Downloads/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://login.yahoo.com/");
+
+
+    }
+}
