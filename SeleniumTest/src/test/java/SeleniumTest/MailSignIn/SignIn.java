e078463ca615601d3901dfe4494374f74b492ab5
poorvaGokhale <poorve@gmail.com>
2020-12-24 14:23:41 +0530

    Inavlid Username Passwd Testcase

--- SeleniumTest/src/test/java/SeleniumTest/MailSignIn/SignIn.java
+++ SeleniumTest/src/test/java/SeleniumTest/MailSignIn/SignIn.java
@@ -1,16 +1,41 @@
 package SeleniumTest.MailSignIn;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SignIn {
     @Test
     public void LaunchWeb(){
-     //   System.setProperty("webdriver.chrome.driver", "/Users/poorvagokhale/Downloads/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://login.yahoo.com/");
+        String webUlr ="https://login.yahoo.com/";
+        String InvalidUserName = "Test";
+        String ValidUserName = "Poorva.Gokhale26";
+        String ValidPasswd = "";
 
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(webUlr);
+//testcase 1 Ivalid username
+/*        WebElement userNameInputtxtBox = driver.findElement(By.id("login-username"));
+        userNameInputtxtBox.sendKeys(InvalidUserName);
+        WebElement signInButton = driver.findElement(By.id("login-signin"));
+        signInButton.submit();
+*/
+        //Valid Username  Invalid Passwd
+        WebElement userNameInputtxtBox = driver.findElement(By.id("login-username"));
+        userNameInputtxtBox.sendKeys(ValidUserName);
+        WebElement signInButton = driver.findElement(By.id("login-signin"));
+        signInButton.submit();
+        WebElement passwdInputBox =driver.findElement(By.id("login-passwd"));
+        passwdInputBox.sendKeys(InvalidUserName);
+
+        signInButton = driver.findElement(By.id("login-signin"));
+        signInButton.submit();
+
+
+        driver.close();
 
     }
 }
